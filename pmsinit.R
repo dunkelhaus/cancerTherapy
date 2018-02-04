c85718512e6959c2cc6f9ef3030a38e697217201
Valeria Brewer <valramirez@ucdavis.edu>
2018-02-03 17:23:11 -0800

    pmsinit.py stores signatures into separate .txt files successfully.

--- pmsinit.R
+++ pmsinit.R
@@ -14,7 +14,23 @@
 library(devtools)
 #devtools::install_github(“friend1ws/pmsignature”)
 library(stats)
-library(pmsignature)
-G <- readMFVFile("/Users/kravisankaran/code/cs193a/cnnCancerTherapy/wrappers/mfv.tab", numBases = 5, type="independent", trDir=TRUE)
-G
-Param <- getPMSignature(G, K = 3)
+library(pmsignature, lib.loc="/home/valeria/R/x86_64-pc-linux-gnu-library/3.2")
+G <- readMFVFile("/home/valeria/ECS193/cnnCancerTherapy/wrappers/mfvmod.tab", numBases = 5, type="independent", trDir=TRUE)
+
+
+#Get command line argument and store it in nums
+myArgs <- commandArgs(trailingOnly = TRUE)
+nums = as.numeric(myArgs)
+
+if(nums == 7) {
+	Param7 <- getPMSignature(G, K = 7)
+	print(Param7)
+}
+if(nums == 13) {
+	Param13 <- getPMSignature(G, K = 13)
+	print(Param13)
+}
+if(nums == 23) {
+	Param23 <- getPMSignature(G, K = 23)
+	print(Param23)
+}
--- pmsinit.py
+++ pmsinit.py
@@ -1,12 +1,36 @@
+# running 'python2.7 pmsinit.py' will produce three .txt files
+#	out7.txt holds signature 7
+#	out13.txt holds signature 13
+#	out23.txt holds signature 23
+
 import subprocess
 
 #Define command and arguments
 command = 'Rscript'
 
-args = ['--default-packages=methods,utils', 'pmsinit.R']
-
+#Specify signature 7
+args = ['--default-packages=methods,utils', 'pmsinit.R', '7']
 cmd = [command] + args
-
 result = subprocess.check_output(cmd, universal_newlines=True)
-
 print(result)
+file7 = open("out7.txt", "w+")
+file7.write(result)
+file7.close()
+
+#Specify signature 13
+args = ['--default-packages=methods,utils', 'pmsinit.R', '13']
+cmd = [command] + args
+result = subprocess.check_output(cmd, universal_newlines=True)
+print(result)
+file7 = open("out13.txt", "w+")
+file7.write(result)
+file7.close()
+
+#Specify signature 23
+args = ['--default-packages=methods,utils', 'pmsinit.R', '23']
+cmd = [command] + args
+result = subprocess.check_output(cmd, universal_newlines=True)
+print(result)
+file7 = open("out23.txt", "w+")
+file7.write(result)
+file7.close()
