78c29f04e7f72143ed02b1718781d4fee2650b1c
Valeria Brewer <valramirez@ucdavis.edu>
2018-02-19 12:58:43 -0800

    Listed needed wrappers.
    Please add any that I have missed.

--- models/dnnClassifier/wrappers.py
+++ models/dnnClassifier/wrappers.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
 
 # Write wrappers for TypeScript files in playground/
+
+""" 
+  Wrappers Needed: 
+        learning rate, 
+        activation function, 
+        regularization, 
+        regularization rate, 
+        problem type, 
+        ratio of training to test data,
+        noise,
+        batch size,
+        which features do you want to feed in,
+        add hidden layer,
+        subtract hidden layer,
+        add neuron to layer[i],
+        remove neuron from layer[i]
+        
+"""
