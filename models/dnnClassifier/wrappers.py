dc4046644971357051cb8a437cce1ae662444c8a
Valeria Brewer <valramirez@ucdavis.edu>
2018-02-19 13:16:41 -0800

    Updated wrappers.py in model. Ready to get started.

--- models/dnnClassifier/wrappers.py
+++ models/dnnClassifier/wrappers.py
@@ -19,3 +19,43 @@
         remove neuron from layer[i]
         
 """
+import json
+
+def learningRateWrapper(object):
+    #check for changes in learning rate    
+
+def activationWrapper(object):
+    #check for changes in activation function
+
+def regularizationWrapper(object):
+    #check for changes in regularization
+
+def regularizationRateWrapper(object):
+    #check for changes in regularization rate
+
+def problemTypeWrapper(object):
+    #check for changes in problem type
+
+def ratioWrapper(object):
+    #check for changes in ratio of training to test data
+
+def noiseWrapper(object):
+    #check for changes in noise
+
+def batchWrapper(object):
+    #check for changes in batch size
+
+def featuresWrapper(object):
+    #check for changes in features to feed in
+
+def addHiddenLayer(object):
+    #check for changes in number of hidden layers (add)
+
+def rmHiddenLayer(object):
+    #check for changes in number of hidden layers (subtract)
+
+def addNeuron(object):
+    #check for changes in neuron count for layer (add)
+
+def rmNeuron(object):
+    #check for changes in neuron count for layer (subtract)
