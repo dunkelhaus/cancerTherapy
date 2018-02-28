cb801603ae8371843f5a080295b9cb2b89c52b52
Suraj Jena <skjena@ucdavis.edu>
2018-02-28 05:07:00 -0800

    updated server + new tunneling protocol

--- server/mutationDnnWeb/mutationDnnWeb/settings.py
+++ server/mutationDnnWeb/mutationDnnWeb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '45$mxn_ry6s$=wk(__#z2ke730wxp=+u#i2ry751ie@8o)8s=*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['62b19883.ngrok.io', 'localhost']
+ALLOWED_HOSTS = ['512e08c6.ngrok.io', 'localhost']
 
 
 # Application definition
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'helloapp',
-    'rest_framework'
+    #'rest_framework'
 ]
 
 MIDDLEWARE = [
