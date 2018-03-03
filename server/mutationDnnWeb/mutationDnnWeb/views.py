949da86ef86d93778dca87fc96fd8d76e200f7b8
Suraj Jena <skjena@ucdavis.edu>
2018-03-03 15:12:43 -0800

    VB: Combined views.py and views2.py into views.py.

--- server/mutationDnnWeb/mutationDnnWeb/views.py
+++ server/mutationDnnWeb/mutationDnnWeb/views.py
@@ -10,53 +10,122 @@ from .serializers import V1Serializer
 #@api_view(['GET', 'POST'])
 class ArgumentList(APIView):
 	def get(self, request):
-		args = Arguments.objects.all()
-		serializer = V1Serializer(args, many=True)
-		return Response(serializer.data)
+		if request.method == 'GET':
+			args = Arguments.objects.all()
+			serializer = V1Serializer(args, many=True)
+			return Response(serializer.data)
+		else:
+			print("ERROR from views.py! GET ")
 
-	def post(self):
-		pass
+	def post(self, request):
+		if request.method == 'POST':
+			args = Arguments.objects.all()
+			serializer = V1Serializer(data=request.data)
+			if serializer.is_Valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                        print("ERROR from views.py! POST ")
 
 #/v1/state/
 #@api_view(['GET', 'POST'])
 class StateList(APIView):
 	def get(self, request):
-		state = State.objects.all()
-		serializer = V1Serializer(state, many=True)
-		return Response(serializer.data)
+                if request.method == 'GET':
+			state = State.objects.all()
+			serializer = V1Serializer(state, many=True)
+			return Response(serializer.data)
+                else:
+                        print("ERROR from views.py! GET ")
 
-	def post(self):
-		pass
+	def post(self, request):
+		if request.method == 'POST':
+			args = State.objects.all()
+			serializer = V1Serializer(data=request.data)
+
+			if serializer.is_Valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
+                else:
+                        print("ERROR from views.py! POST ")
+
 
 #/v1/run/
 #@api_view(['GET', 'POST'])
 class RunList(APIView):
 	def get(self, request):
-		run = Run.objects.all()
-		serializer = V1Serializer(run, many=True)
-		return Response(serializer.data)
+                if request.method == 'GET':
+			run = Run.objects.all()
+			serializer = V1Serializer(run, many=True)
+			return Response(serializer.data)
+                else:
+                        print("ERROR from views.py! GET ")
 
-	def post(self):
-		pass
+
+	def post(self, request):
+		if request.method == 'POST':
+			args = Run.objects.all()
+			serializer = V1Serializer(data=request.data)
+
+			if serializer.is_Valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                        print("ERROR from views.py! POST ")
+
 
 #/v1/features/
 #@api_view(['GET', 'POST'])
 class FeatureList(APIView):
 	def get(self, request):
-		features = Features.objects.all()
-		serializer = V1Serializer(features, many=True)
-		return Response(serializer.data)
+                if request.method == 'GET':
+			features = Features.objects.all()
+			serializer = V1Serializer(features, many=True)
+			return Response(serializer.data)
+                else:
+                        print("ERROR from views.py! GET ")
 
-	def post(self):
-		pass
+	
+	def post(self, request):
+		if request.method == 'POST':
+			args = Features.objects.all()
+			serializer = V1Serializer(data=request.data)
+
+			if serializer.is_Valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                        print("ERROR from views.py! POST ")
+
 
 #/v1/settings/
 #@api_view(['GET', 'POST'])
 class SettingsList(APIView):
 	def get(self, request):
-		settings = Settings.objects.all()
-		serializer = V1Serializer(settings, many=True)
-		return Response(serializer.data)
+                if request.method == 'GET':
+			settings = Settings.objects.all()
+			serializer = V1Serializer(settings, many=True)
+			return Response(serializer.data)
+                else:
+                        print("ERROR from views.py! GET ")
 
-	def post(self):
-		pass
+	def post(self, request):
+		if request.method == 'POST':
+			args = Features.objects.all()
+			serializer = V1Serializer(data=request.data)
+
+			if serializer.is_Valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                        print("ERROR from views.py! POST ")
+
