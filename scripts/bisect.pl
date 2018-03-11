fa972165d41e16fbc93ea07acdff0ba3a70ed23f
Suraj Jena <jena.suraj.k@gmail.com>
2018-03-10 17:54:01 -0800

    script update; loop considers header and first line

--- scripts/bisect.pl
+++ scripts/bisect.pl
@@ -12,15 +12,22 @@ $samplefile = shift;
 open(TAB, "<$samplefile") or die "Can't open file $samplefile \n";
 
 $count = 0;
-open(my $fh, '>>', 'headerline.out'); # Configure out file
+open(my $headerline, '>>', 'headerline.out'); # Configure out file for headerline
+open(my $lungadL1, '>>', 'lungadL1.out'); # Configure out file for lungadL1
+
 while($line = <TAB>) {
-    if ($count < 1) {
-        print $fh $line;
-        close $fh;
+    if ($count == 0) { # This is for the header line
+        print $headerline $line;
+        close $headerline;
+        $count++;
+    }
+    if ($count == 1) { # This is for the header line
+        print $lungadL1 $line;
+        close $lungadL1;
         $count++;
     }
     else {
-        print "Running through other lines here."
+        print "Running through other lines here. \n"
     }
 }
 print "Done. \n";
