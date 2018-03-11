54aa3adcb39c0a411875618a9e7992f0ae7fe891
Suraj Jena <jena.suraj.k@gmail.com>
2018-03-10 17:11:12 -0800

    Initial bisect script in perl

--- scripts/bisect.pl
+++ scripts/bisect.pl
@@ -1,1 +1,16 @@
+#!/usr/bin/perl
+
 # Bisect fm_sample_independent.tab
+# Filename provided in command line
+
+# Usage: bisect.pl fm_sample_independent.tab > out-file
+
+use bytes;
+
+# Take input from file
+$samplefile = shift;
+open(TAB, "<$samplefile") or die "Can't open file $samplefile";
+
+while($sample = <TAB>) {
+    print "Sample is: $sample";
+}
