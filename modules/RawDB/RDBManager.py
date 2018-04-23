40d371634d86074a00dc049cbe29cc68d9cd4efb
Suraj Jena <jena.suraj.k@gmail.com>
2018-04-23 02:57:47 +0000

    Implements GETCSV(), which outputs a CSV file in the specified path

--- modules/RawDB/RDBManager.py
+++ modules/RawDB/RDBManager.py
@@ -1,1 +1,8 @@
-#
+from IE import CreateCSV
+
+# GETCSV creates a csv file in the specified path
+# @row: a tag, specifies whether a row is a cancer or a token
+# @column: a tag, specifies whether a column is a cancer or a token
+
+def GETCSV(row, column, numRows, numCols, path):
+    CreateCSV.createCSV(row, column, numRows, numCols, path)
