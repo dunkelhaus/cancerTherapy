c952c190d3797bda136c20dbf3a3ec01ccd26978
Suraj Jena <jena.suraj.k@gmail.com>
2018-04-23 23:18:53 +0000

    reads from /home/skjena/data/fm_sample_independent.tab

--- modules/RawDB/scripts/convertToCSV.sh
+++ modules/RawDB/scripts/convertToCSV.sh
@@ -9,7 +9,7 @@ $5 is path
 '
 
 DIR=$5
-cat fm_sample_independent.tab | tail -n +2 > f1.tab
+cat /home/skjena/data/fm_sample_independent.tab | tail -n +2 > f1.tab
 sed -n 1,$3p f1.tab > f.tab
 awk -v f=1 -v t=$4 '{for(i=f;i<=t;i++) printf("%s%s",$i,(i==t)?"\n":OFS)}' f.tab > file.tab
 cat file.tab | tr "\ " "," > "$DIR/data.csv"
