53194a169a04805d87514d128da80065c62cc548
Service Account <skjena@deb-8-data-analysis.c.cnncancertherapy.internal>
2018-04-24 23:49:47 +0000

    separated into example and feature

--- modules/NeuralNet/classifiers/dnnClassifier/exampleCSVRead.py
+++ modules/NeuralNet/classifiers/dnnClassifier/exampleCSVRead.py
@@ -6,21 +6,22 @@ filename_queue = tf.train.string_input_producer(["data.csv"])
 reader = tf.TextLineReader()
 key, value = reader.read(filename_queue)
 
-record_defaults = [[1], [1], [1], [1], [1], [1], [1], [1]]
+record_defaults = [tf.as_string([1]), [1], [1], [1], [1], [1], [1], [1]]
 col1, col2, col3, col4, col5, col6, col7, col8 = tf.decode_csv(
     value, record_defaults=record_defaults)
-features = tf.stack([col1, col2, col3, col4, col5, col6, col7, col8])
+features = tf.stack([col2, col3, col4, col5, col6, col7, col8])
 
 with tf.Session() as sess:
   # Start populating the filename queue.
   coord = tf.train.Coordinator()
   threads = tf.train.start_queue_runners(coord=coord)
 
-  for i in range(9):
+  for i in range(0,8):
     # Retrieve a single instance:
     try:
-        example, label = sess.run([features, col8])
+        example, label = sess.run([features, col1])
         print(example)
+        print(label)
     except tf.errors.OutOfRangeError:
         break
 
