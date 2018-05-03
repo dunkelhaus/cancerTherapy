cc2ec66eabcd27d57cbfd00a188fdbda0cfa6a93
Valeria Brewer <valramirez@ucdavis.edu>
2018-05-03 16:58:29 +0000

    Update DDManager.py to use 2 separate Queues.
    One queue for testing folds and another for training folds.

--- modules/DataDispatcher/DDManager.py
+++ modules/DataDispatcher/DDManager.py
@@ -2,20 +2,25 @@
 """
     ** DispatchData() will:
         - be called from CVManager asynchronously
-        - take a queue as an argument
+        - take 2 queues as arguments: one for testing and one for training
         - pass folds to network while new folds are being generated
         s(no time wasted waiting for all folds to be generated before sending the first one)
 """
 from Queue import *
 import time
 
-def DispatchData(queue):
-    while queue.empty() == True:
-        #wait for data to be added to queue
+def DispatchData(TestingQueue, TrainingQueue):
+    while TestingQueue.empty() == True and TrainingQueue.empty() == True:
+        #wait for data to be added to either queue
         time.sleep(1)
-    while queue.empty() == False:
+    while TestingQueue.empty() == False or TrainingQueue.empty() == False:
         #once data is in queue, send it off to network
-        print 'Empty DataDispatcher Queue?: ', queue.empty() #just for visualization
-        fold = queue.get() #pop from queue until it is empty
+        print 'Empty Testing Queue?: ', TestingQueue.empty() #just for visualization
+        print 'Empty Training Queue?: ', TrainingQueue.empty() #just for visualization
+        if TestingQueue.empty() == False:
+            TestFold = TestingQueue.get() #pop from Testing queue until it is empty
+        if TrainingQueue.empty() == False:
+            TrainFold = TrainingQueue.get() #pop from Training queue until it is empty
         #TODO: send fold to network
-    print 'Empty DataDispatcher Queue?: ', queue.empty() #just for visualization
+    print 'Empty Testing Queue?: ', TestingQueue.empty() #just for visualization
+    print 'Empty Training Queue?: ', TrainingQueue.empty() #just for visualization
