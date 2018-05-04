1f2bd9458a07b48f8df2e89173dba08157f5c01d
Service Account <skjena@deb-8-data-analysis.c.cnncancertherapy.internal>
2018-05-04 13:14:34 +0000

    headers included now

--- modules/RawDB/scripts/convertToCSV.sh
+++ modules/RawDB/scripts/convertToCSV.sh
@@ -10,12 +10,13 @@ $5 is path
 
 DIR=$5
 negOne=-1
-cat /home/skjena/data/fm_sample_independent.tab | tail -n +2 > f1.tab
-sed -n 1,$3p f1.tab > f2.tab
+#cat /home/skjena/data/fm_sample_independent.tab | tail -n +2 > f1.tab
+#sed -n 1,$3p f1.tab > f2.tab
+sed -n 1,$3p /home/skjena/data/fm_sample_independent.tab > f2.tab
 sed -e "s/NA/$negOne/" f2.tab > f.tab
 awk -v f=1 -v t=$4 '{for(i=f;i<=t;i++) printf("%s%s",$i,(i==t)?"\n":OFS)}' f.tab > file.tab
 cat file.tab | tr "\ " "," > "$DIR/data.csv"
 rm file.tab
-rm f1.tab
+#rm f1.tab
 rm f2.tab
 rm f.tab
