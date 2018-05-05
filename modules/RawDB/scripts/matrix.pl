a7569d15496d72285ccfcef666549691a53d685a
Kumud Ravisankaran <kravisankaran@ucdavis.edu>
2018-05-04 17:13:44 -0700

    Matrix generator

---
+++ modules/RawDB/scripts/matrix.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Text::CSV;
+
+my @data;   # 2D array for CSV data
+my $file = 'data.csv';
+
+my $csv = Text::CSV->new;
+open my $fh, '<', $file or die "Could not open $file: $!";
+
+while( my $row = $csv->getline( $fh ) ) {
+    shift @$row;        # throw away first value
+    push @data, $row;
+
+}
+foreach my $row (@data) {
+    foreach my $element (@$row) {
+        print $element, ' '
+    }
+    print "\n"
+}
+#print $data[0][1], "\n";
