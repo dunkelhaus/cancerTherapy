d2ed00bedb4f6f3cdc9816146d1e4a2daeec9f85
Service Account <skjena@deb-8-data-analysis.c.cnncancertherapy.internal>
2018-05-07 23:03:39 +0000

    Pending completion of NNManager

--- modules/IndexedDB/IDBManager.py
+++ modules/IndexedDB/IDBManager.py
@@ -1,1 +1,14 @@
-#
+import sys
+sys.path.append('../')
+sys.dont_write_bytecode = True
+
+from RawDB.RDBManager import GETCSV
+from CrossValidation.CVManager import CrossValidate
+#from NeuralNet.management.NNManager import modelZero
+
+def IDB(path):
+    GETCSV("CANCER", "TOKEN", 21, 21, path)
+    path = path + "data.csv"
+    CrossValidate(path)
+
+IDB("/home/skjena/cancerTherapy/modules/IndexedDB/")
