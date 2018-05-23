eeb587b5294d83e489725243134df2667262bba8
Suraj Jena <skjena@ucdavis.edu>
2018-05-23 14:12:38 -0700

    Updates to admin

--- modules/Admin/Admin.py
+++ modules/Admin/Admin.py
@@ -22,6 +22,10 @@ class Admin:
         self.indexedDb = IDBManager(self.origin, self.numFolds)
         self.foldset = self.indexedDb.createFolds()
         self.crossValidate = CVManager(self.foldset)
+        self.dataDispatcher = DDManager()
+        self.neuralNets = Queue()
+        for i in range(0, self.numFolds)
+            self.neuralNets.put(NNManager(self.dataDispatcher.trainPaths.get(), self.dataDispatcher.testPaths.get(), self.restApi.network)
 
     def initialize():
         self.status.message(1,"initialize()")
@@ -30,11 +34,11 @@ class Admin:
         self.CV = CVManager("/home/skjena/cancerTherapy/modules/RawDB/scripts/fm_mutations_independent.csv", self.numFolds)
         #Call CVManager's CrossValidate function which returns a list of fold-paths
         self.foldList = self.CV.CrossValidate("/home/skjena/cancerTherapy/modules/RawDB/scripts/fm_mutations_independent.csv",self.numFolds)
-        
+
         #Start crossValidation process separately
         self.crossValidate = Process(target=self.CV.validation, args=(self.foldList,self.DDQTesting,self.DDQTraining))
         self.crossValidate.start()
-        
+
         #Pop from testing and training queues until empty and pass paths to NN
         for i in range(1,self.numFolds+1):
             self.dispatchData = Process(target=self.DD.DispatchData)
@@ -43,8 +47,6 @@ class Admin:
 
         self.status.message(0,"initialize()")
         return
-   
+
     def build(self):
         self.status.message(1, "build(self)")
-        
-         
